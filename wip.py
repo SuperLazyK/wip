8e2890c942647ff44ae1b5544c8329f72f2fe506
none <none>
2023-05-24 07:02:34 +0900

    bk

--- wip.py
+++ wip.py
@@ -27,20 +27,30 @@ context = { l: 0.5, r: 0.05,
         g: 9.81
         }
 
+vy = False
+if vy:
+    IDX_W = 1
+    IDX_Y = 0
+else:
+    IDX_W = 0
+
 class WIPG(LinkTreeModel):
 
     def __init__(self, simp=True):
         # initial wheel angle should be vertical
-        jl0 = StickJointLink("y", 0, 0, PrismaticJoint(), XT=Xpln(-pi/2, 0, 0), Icog=0)
-        jl1 = WheelJointLink("qw", mw, r, RackPinionJoint(r, x0), XT=Xpln(pi/2, 0, 0), Icog=Iw)
-        jl2 = StickJointLink("qs", mb, l, RevoluteJoint(), I=transInertia(mcI(mb, [l, 0], Ib), Xpln(0, 0, 0)), tau=uw)
-        super().__init__([jl0, jl1, jl2], g, X0=Xpln(pi/2, 0, 0))
+        if vy:
+            jl0 = StickJointLink("y", 0, 0, PrismaticJoint(), XT=Xpln(-pi/2, 0, 0), Icog=0)
+            jl1 = WheelJointLink("qw", mw, r, RackPinionJoint(r, x0), XT=Xpln(pi/2, 0, 0), Icog=Iw)
+            jl2 = StickJointLink("qs", mb, l, RevoluteJoint(), I=transInertia(mcI(mb, [l, 0], Ib), Xpln(0, 0, 0)), tau=uw)
+            super().__init__([jl0, jl1, jl2], g, X0=Xpln(pi/2, 0, 0))
+        else:
+            jl1 = WheelJointLink("qw", mw, r, RackPinionJoint(r, x0), XT=Xpln(pi/2, 0, 0), Icog=Iw)
+            jl2 = StickJointLink("qs", mb, l, RevoluteJoint(), I=transInertia(mcI(mb, [l, 0], Ib), Xpln(0, 0, 0)), tau=uw)
+            super().__init__([jl1, jl2], g, X0=Xpln(0, 0, 0))
         self.gen_function(context)
         self.reset()
 
     def reset(self):
-        self.q_v = np.array([0, 0, 0])
-        self.dq_v = np.array([0, 0, 0])
         self.v_ref = 0 # horizontal velocity
         self.x0_v = 0
         self.v_uw = 0
@@ -58,18 +68,16 @@ class WIPG(LinkTreeModel):
         return [self.x0_v]
 
     def feedback(self):
-        IDX_W = 1
-        IDX_Y = 0
         K = np.array([[-1, 41.26540066, 125.12381105]])
         v_uw = wip_wheel_torq(K, self.v_ref, self.q_v[IDX_W:], self.dq_v[IDX_W:], 0)
         self.v_uw = v_uw
         b = 400.
         k = b * b / 4*(context[mb] + context[mw]) # zeta == 1
-        print(self.q_v[IDX_Y])
-        if self.q_v[IDX_Y] < 0:
-            self.fext_v[IDX_W][2] =-k * self.q_v[IDX_Y] -b * self.dq_v[IDX_Y]
-        else:
-            self.fext_v[IDX_W][2] = 0
+        if vy:
+            if self.q_v[IDX_Y] < 0:
+                self.fext_v[IDX_W][2] =-k * self.q_v[IDX_Y] -b * self.dq_v[IDX_Y]
+            else:
+                self.fext_v[IDX_W][2] = 0
 
 def test():
     model_g = WIPG()
